c00fbdbcae3634d3754170f68fe6e43a7cd86187
Huanan <afuture0@outlook.com>
2025-08-29 22:17:14 +0800

    extract message content from reponse

--- Sources/GPT/Public/Content+MessageItem.swift
+++ Sources/GPT/Public/Content+MessageItem.swift
@@ -6,7 +6,60 @@
 //
 
 
-// MARK: Message
+/// An enumeration of the types of content that can be included in a message item.
+public enum MessageContent: Sendable {
+
+    /// Text-based content.
+    case text(TextGeneratedContent)
+    /// A refusal to provide content.
+    case refusal(TextRefusalGeneratedContent)
+}
+
+extension MessageContent {
+    public var text: TextGeneratedContent? {
+        guard case let .text(value) = self else {
+            return nil
+        }
+        return value
+    }
+    
+    public var refusal: TextRefusalGeneratedContent? {
+        guard case let .refusal(value) = self else {
+            return nil
+        }
+        return value
+    }
+}
+
+extension MessageContent: Codable {
+    enum CodingKeys: String, CodingKey {
+        case type
+    }
+    
+    public init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let type = try container.decode(GeneratedContentType.self, forKey: .type)
+        switch type {
+        case .generatedText:
+            self = try .text(.init(from: decoder))
+        case .generatedTextRefusal:
+            self = try .refusal(.init(from: decoder))
+        default:
+            throw DecodingError.typeMismatch(MessageItem.self, .init(codingPath: [], debugDescription: "Only Support 'MessageItem'"))
+        }
+    }
+    
+    public func encode(to encoder: any Encoder) throws {
+        var container = encoder.singleValueContainer()
+        switch self {
+        case .text(let value):
+            try container.encode(value)
+        case .refusal(let value):
+            try container.encode(value)
+        }
+    }
+}
+
 
 extension GeneratedContentType {
     /// The content type for a message item.
@@ -20,7 +73,7 @@ public struct MessageItem: Identifiable, Sendable, Codable {
     public let index: Int?
     
     /// The content of the message, which can be text or a refusal.
-    public let content: [ResponseContent]?
+    public let content: [MessageContent]?
     
     enum CodingKeys: CodingKey {
         case id
@@ -29,7 +82,7 @@ public struct MessageItem: Identifiable, Sendable, Codable {
         case content
     }
 
-    public init(id: String, index: Int?, content: [ResponseContent]?) {
+    public init(id: String, index: Int?, content: [MessageContent]?) {
         self.id = id
         self.index = index
         self.content = content
