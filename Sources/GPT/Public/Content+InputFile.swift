a4b3695e04b444c3d4679289dae9b262ce6d1ee7
Huanan <afuture.d@outlook.com>
2025-08-29 14:27:18 +0800

    rebuild content types

---
+++ Sources/GPT/Public/Content+InputFile.swift
@@ -0,0 +1,50 @@
+//
+//  Content+FileInput.swift
+//  swift-gpt
+//
+//  Created by Huanan on 2025/8/29.
+//
+
+// MARK: File
+
+extension ContentType {
+    static let inputFile = ModelInputContentType(rawValue: "File")
+}
+
+/// A file-based input for a prompt.
+public struct FileInputContent: ModelInputContent, Codable {
+
+    public let type: ModelInputContentType = .inputFile
+    /// The role of the entity providing the content.
+    public let role: ModelContentRole
+
+    /// An optional identifier for the file.
+    public let id: String?
+
+    /// An optional filename for the file.
+    public let filename: String?
+
+    /// The content of the file, typically base64-encoded.
+    public let content: String
+
+    enum CodingKeys: CodingKey {
+        case type
+        case role
+        case id
+        case filename
+        case content
+    }
+
+    /// Creates a new file content item.
+    /// - Parameters:
+    ///   - role: The role of the entity providing the content.
+    ///   - id: An optional identifier for the file.
+    ///   - filename: An optional filename for the file.
+    ///   - content: The content of the file.
+    public init(role: ModelContentRole, id: String?, filename: String?, content: String) {
+        self.role = role
+        self.id = id
+        self.filename = filename
+        self.content = content
+    }
+}
