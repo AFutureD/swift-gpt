811b38a59173bcf31c4ea03d0073b2b685287040
Huanan <afuture0@outlook.com>
2025-08-31 03:40:03 +0800

    add openai compatiable request convert

--- Sources/GPT/Provider/Vendor/OpenAICompatible/Convert+OpenAICompatible.swift
+++ Sources/GPT/Provider/Vendor/OpenAICompatible/Convert+OpenAICompatible.swift
@@ -5,45 +5,97 @@ import LazyKit
 import SynchronizationKit
 
 
-extension OpenAIChatCompletionRequestMessageContentPart {
+extension OpenAIChatCompletionRequestMessage {
     init?(_ input: Prompt.Input) {
         switch input {
-        case .text(let text):
-            self = .text(.init(text: text.content))
-        case .file(let file):
-            self = .file(.init(file: .init(fileId: file.id, filename: file.filename, fileData: file.content)))
+            case .text(let text):
+                let part: OpenAIChatCompletionRequestMessageContentPart = .text(.init(text: text.content))
+                switch text.role {
+                case .system:
+                    self = .system(.init(content: .parts([part]), name: nil))
+                case .assistant:
+                    self = .assistant(.init(audio: nil, content: .parts([part]), name: nil, refusal: nil, tool_calls: nil))
+                case .user:
+                    self = .user(.init(content: .parts([part]), name: nil))
+                case .developer:
+                    self = .developer(.init(content: .parts([part]), name: nil))
+                default:
+                    return nil
+                }
+            case .file(let file):
+                let part: OpenAIChatCompletionRequestMessageContentPart = .file(.init(file: .init(fileId: file.id, filename: file.filename, fileData: file.content)))
+                switch file.role {
+                case .system:
+                    self = .system(.init(content: .parts([part]), name: nil))
+                case .assistant:
+                    self = .assistant(.init(audio: nil, content: .parts([part]), name: nil, refusal: nil, tool_calls: nil))
+                case .user:
+                    self = .user(.init(content: .parts([part]), name: nil))
+                case .developer:
+                    self = .developer(.init(content: .parts([part]), name: nil))
+                default:
+                    return nil
+                }
         }
     }
 }
 
+extension OpenAIChatCompletionRequestMessage {
+    init?(_ item: GeneratedItem) {
+        switch item {
+        case .message(let message):
+            let refusal: String? = message.content?.map {
+                if case let .refusal(refusal) =  $0 {
+                    return refusal.content
+                }
+                return nil
+            }.first ?? nil
+
+            let parts: [OpenAIChatCompletionRequestMessageContentPart] = message.content?.compactMap { content in
+                switch content {
+                case .text(let text):
+                    if let text = text.content {
+                        return OpenAIChatCompletionRequestMessageContentPart.text(OpenAIChatCompletionRequestMessageContentTextPart.init(text: text))
+                    }
+                    return nil
+                case .refusal(let refusal):
+                    if let refusal = refusal.content {
+                        return OpenAIChatCompletionRequestMessageContentPart.refusal(OpenAIChatCompletionRequestMessageContentRefusalPart.init(refusal: refusal))
+                    }
+                    return nil
+                }
+            } ?? []
+
+            self = .assistant(.init(audio: nil, content: .parts(parts), name: nil, refusal: refusal, tool_calls: nil))
+        }
+        return nil
+    }
+}
+
+
 extension OpenAIChatCompletionRequest {
-    init(_ prompt: Prompt, model: String, stream: Bool) {
+    init(_ prompt: Prompt, conversation: Conversation, model: String, stream: Bool) {
         var messages: [OpenAIChatCompletionRequestMessage] = []
         
         if let instruction = prompt.instructions {
             messages.append(.system(.init(content: .text(instruction), name: nil)))
         }
-        
-        let inputs: [OpenAIChatCompletionRequestMessage] = prompt.inputs.chunked(
-            on: \.content.role
-        ).compactMap { role, inputs in
-            let parts = inputs.compactMap {
-                OpenAIChatCompletionRequestMessageContentPart($0)
-            }
-            switch role {
-            case .system:
-                return .system(.init(content: .parts(parts), name: nil))
-            case .assistant:
-                return .assistant(.init(audio: nil, content: .parts(parts), name: nil, refusal: nil, tool_calls: nil))
-            case .user:
-                return .user(.init(content: .parts(parts), name: nil))
-            case .developer:
-                return .developer(.init(content: .parts(parts), name: nil))
-            default:
-                return nil
+
+        let items = conversation.items
+        for item in items {
+            switch item {
+            case .input(let input):
+                let message = OpenAIChatCompletionRequestMessage(input)
+                if let message {
+                    messages.append(message)
+                }
+            case .generated(let generated):
+            let message = OpenAIChatCompletionRequestMessage(generated)
+                if let message {
+                    messages.append(message)
+                }
             }
         }
-        messages.append(contentsOf: inputs)
 
         self.init(
             messages: messages,
